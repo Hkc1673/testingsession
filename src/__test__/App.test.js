1befa521c0ab90495c7bf63316eb393148010c04
habib koc <habib.koc@faturakom.com>
2021-06-05 22:36:05 +0300

    second

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,10 +1,35 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
+import { render, screen } from "@testing-library/react";
+import App from "../App";
 
-test('Logo must have src and alt attribute', () => {
- render(<App/>);
- screen.debug();
+beforeEach(() => {
+  render(<App />);
+});
 
- const logo = screen.getByRole("img");
- expect(logo).toHaveAttribute("src", "clarusway_logo.png")
+test("Logo must have src and alt attribute", () => {
+  //  render(<App/>);
+  screen.debug();
+
+  const logo = screen.getByRole("img");
+  expect(logo).toHaveAttribute("src", "clarusway_logo.png");
+  expect(logo).toHaveAttribute("alt", "Clarusway");
+});
+
+test("header renders with correct text", () => {
+  // render(<App/>);
+  const headerElement = screen.getByText("Clarusway News App");
+  // expect(headerElement).toBeInTheDocument()
+  expect(headerElement.textContent).toBe("Clarusway News App");
+});
+
+test("search text and button text should be in app", () => {
+  const searchElement = screen.getByText(/Search/);
+  expect(searchElement).toBeInTheDocument();
+
+  const buttonText = screen.getByText(/Go to news/);
+  expect(buttonText).toBeInTheDocument();
+});
+
+test("input contains initial value", () => {
+  const inputElement = screen.getByTestId("input");
+  expect(inputElement).toHaveValue("");
 });
